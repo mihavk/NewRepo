d241215379cfa21865d34a78988c0a3a2cf38786
mihavk <Mihavk2001@gmail.com>
2020-09-18 11:23:32 +0300

    2

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] text ={"Den",
+        "LOVE",
+        "ВАЗЕЛИН",
+        "CONTEX",
+        "C++",
+        " +79263772622",
+        " 89263772622",
+        " (926)3772622" };
+
+      //string str56 = "c:\vazelin\artem\nude_photos";
+      string str56 = $"stringi {text[0]}A";
+      Console.WriteLine(str56);
+
+
+      foreach (string item in text)
+      {
+        string pattern = @"\d{7}";
+        if (Regex.IsMatch(item, pattern))
+        {
+          //Console.WriteLine($"{item} это телефон"); 
+          Console.WriteLine(item + " это телефон");
+        }
+        else
+        {
+          Console.WriteLine(
+          string.Format("{0} это НЕ телефон", item)
+          );
+        }
+
+      }
+
+    }
+  }
+}
---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace prak4_02_upr02
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Stack stack = new Stack();
+      stack.Push("1");
+      stack.Push("2");
+      stack.Push("TRUB");
+      stack.Push("4");
+      while (stack.Count > 0)
+      {
+        object obj = stack.Pop();
+        Console.WriteLine("'From Stack: {0}", obj);
+      }
+
+    }
+  }
+}
